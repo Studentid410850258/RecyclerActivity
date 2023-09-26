be28eed61b95140ae5408e965dd6c3530848b216
Studentid410850258 <134401041+Studentid410850258@users.noreply.github.com>
2023-09-27 00:24:15 +0800

    change

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,7 +2,10 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +16,9 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         //Step 2: Provide a LayoutManager
-
-
+        val LayoutManager=LinearLayoutManager(this)
         //Step 4: Provide a RecyclerView.Adapter
+        val adapter=NumberDisplayAdapter()
+        recyclerView.adapter=adapter
     }
 }
